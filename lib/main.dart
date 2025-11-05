9d5720caf3a3e919de34ea09383b3e939317e7b4
rana7azem <ranahazem816@gmail.com>
2025-11-05 19:16:11 +0200

    Added bottom navigation and working app structure

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'theme/app_theme.dart';
+import 'widgets/bottom_nav_bar.dart';
+
+void main() {
+  runApp(const FoodRecipesApp());
+}
+
+class FoodRecipesApp extends StatefulWidget {
+  const FoodRecipesApp({Key? key}) : super(key: key);
+
+  @override
+  State<FoodRecipesApp> createState() => _FoodRecipesAppState();
+}
+
+class _FoodRecipesAppState extends State<FoodRecipesApp> {
+  bool _isDark = false;
+
+  void toggleTheme(bool value) {
+    setState(() {
+      _isDark = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Recipes App',
+      debugShowCheckedModeBanner: false,
+      theme: _isDark ? AppTheme.darkTheme : AppTheme.lightTheme,
+      home: const BottomNavBar(),
+    );
+  }
+}
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../screens/home_screen.dart';
+import '../screens/profile_screen.dart';
+import '../screens/settings_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+  
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const ProfileScreen(),
+    const SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
